82a6b59d3be09a85ff2d1371a69818baea69ce46
Ali <m.alimbanza@gmail.com>
2024-06-29 08:32:01 +0100

    initialisation

---
+++ models/partenaire.model.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class partenaire extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  partenaire.init({
+    denomination: DataTypes.STRING,
+    id_national : DataTypes.STRING,
+    rccm_f      : DataTypes.STRING,
+    telephone   : DataTypes.STRING,
+    email       : DataTypes.STRING,
+    autorisation: DataTypes.STRING,
+    adresse_physique: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'partenaire',
+  });
+  return partenaire;
+};
