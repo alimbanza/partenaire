6b4616897d0a2b55dfe58141fd43a034be20fdd9
ali <m.alimbanza@mail.com>
2024-09-01 19:51:04 +0200

    mise à jour

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class user extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,15 +13,34 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    nom: DataTypes.STRING,
-    email: DataTypes.STRING,
-    groupe_id: DataTypes.INTEGER,
-    etablissement_id: DataTypes.INTEGER,
-    mot_de_passe:DataTypes.STRING
+  user.init({
+    prenom             :DataTypes.STRING,
+    nom                :DataTypes.STRING,
+    post_nom           :DataTypes.STRING,
+    sexe               :DataTypes.STRING,
+    date_naissance     :DataTypes.STRING,
+    lieu_naissance     :DataTypes.STRING,
+    etat_civil         :DataTypes.INTEGER,
+    id_type            :DataTypes.INTEGER,
+    email              :{
+                            type: DataTypes.TEXT,
+                            allowNull: false,
+                            unique: true
+                        },
+    telephone          :DataTypes.STRING,
+    pseudo             :DataTypes.STRING,
+    motpasse           :DataTypes.STRING,
+    etat_mot_de_passe  :DataTypes.STRING,
+    numero_piece       :DataTypes.STRING,
+    id_etat_civil      :DataTypes.INTEGER,
+    adresse_physique   :DataTypes.STRING,
+    photo              :DataTypes.STRING,
+    id_type_piece_ident:DataTypes.INTEGER,
+    id_nationalite     :DataTypes.INTEGER,
+    id_province        :DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'user',
   });
-  return User;
+  return user;
 };
--- routes/auth.route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const {signIn} = require('../controllers/AuthController');
-
-router.post('/',signIn);
-
-module.exports = router;
-
-
